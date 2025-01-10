ef97eaf554998ef74414a8778733488e70772ace
Rajip2001 <Rajiptuitui272@gmail.com>
2025-01-10 21:21:39 +0545

    final commit

--- linear_interpolation.py
+++ linear_interpolation.py
@@ -1,14 +1,14 @@
-# Linear interpolation in python
 
-print('Enter first point:') # Reading first point
+
+print('Enter first point:')
 x0 = float(input('x0 = '))
 y0 = float(input('y0 = '))
 
-print('Enter second point:') # Reading second point
+print('Enter second point:') 
 x1 = float(input('x1 = '))
 y1 = float(input('y1 = '))
 
-# Reading calculation point
+
 xp = float(input('Enter calculation point xp: '))
 
 yp = y0 + ((y1-y0)/(x1-x0)) * (xp - x0)
